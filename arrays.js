4ef30da65747d223c2a4f30c56383141cbd532f4
EHaig <info@elianahaig.com>
2017-10-16 14:43:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 chocolateBars;
 
 function addElementToBeginningOfArray(array, element) {
