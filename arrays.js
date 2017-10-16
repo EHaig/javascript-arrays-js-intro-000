a93f79e3688e50ca144c6efb7fc1b784e27e114a
EHaig <info@elianahaig.com>
2017-10-16 14:43:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+chocolateBars;
+
+function addElementToBeginningOfArray(array, element) {
+return[...chocolateBars, array, element];
+}
+//
+function destructivelyAddElementToBeginningOfArray (array, element) {
+return newArray = [chocolateBars.unshift(array, element)];
+}
