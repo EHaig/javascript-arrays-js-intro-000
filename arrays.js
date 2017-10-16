608ed8c4f632889d1e0033cc4f717ffc45e2c577
EHaig <info@elianahaig.com>
2017-10-16 14:48:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ chocolateBars;
 function addElementToBeginningOfArray(array, element) {
 return[...chocolateBars, array, element];
 }
-addElementToBeginningOfArray('foo', 1);
+addElementToBeginningOfArray();
 //
 function destructivelyAddElementToBeginningOfArray (array, element) {
 return newArray = [chocolateBars.unshift(array, element)];
